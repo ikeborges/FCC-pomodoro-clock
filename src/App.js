6973b05812c667a5a1769cdea790bf8f0f146e69
Kaique Borges <rique.kaique@gmail.com>
2022-05-12 01:09:21 -0300

    Implement Break and Session lengths

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
+import { useState } from 'react'
 import './App.css'
 
+function useClockOptionLength(startingValue) {
+  const [value, setValue] = useState(startingValue)
+
+  function setClockOptionLength(newLength) {
+    if (newLength > 0 && newLength < 60) setValue(newLength)
+  }
+
+  return [value, setClockOptionLength]
+}
+
 function App() {
+  const [breakLength, setBreakLength] = useClockOptionLength(5)
+  const [sessionLength, setSessionLength] = useClockOptionLength(25)
+
+  function resetClickHandler() {}
+
+  function togglePlayingHandler() {}
+
   return (
     <div className="app">
-      <h1 className="app-title">25+5 Clock</h1>
+      <h1>25+5 Clock</h1>
       <div className="clock-options">
         <div className="clock-option">
           <h2 id="break-label">Break Length</h2>
           <div className="number-control">
-            <button id="break-decrement" className="decrease">
+            <button
+              onClick={() => setBreakLength(breakLength - 1)}
+              id="break-decrement"
+            >
               -
             </button>
-            <span id="break-length">5</span>
-            <button id="break-increment" className="increse">
+            <span id="break-length">{breakLength}</span>
+            <button
+              onClick={() => setBreakLength(breakLength + 1)}
+              id="break-increment"
+            >
               +
             </button>
           </div>
@@ -20,11 +44,19 @@ function App() {
         <div className="clock-option">
           <h2 id="session-label">Session Length</h2>
           <div className="number-control">
-            <button id="session-decrement" className="decrease">
+            <button
+              onClick={() => setSessionLength(sessionLength - 1)}
+              id="session-decrement"
+              className="decrease"
+            >
               -
             </button>
-            <span id="session-length">25</span>
-            <button id="session-increment" className="increse">
+            <span id="session-length">{sessionLength}</span>
+            <button
+              onClick={() => setSessionLength(sessionLength + 1)}
+              id="session-increment"
+              className="increse"
+            >
               +
             </button>
           </div>
@@ -34,10 +66,10 @@ function App() {
         <h2 id="timer-label">Session</h2>
         <p id="time-left">25:00</p> {/* Note the format MM:SS */}
         <div className="clock-controls">
-          <button id="start_stop" className="clock-controls--start">
+          <button onClick={togglePlayingHandler} id="start_stop">
             Start/Stop
           </button>
-          <button id="reset" className="clock-controls--reset">
+          <button onClick={resetClickHandler} id="reset">
             Reset
           </button>
           <audio
