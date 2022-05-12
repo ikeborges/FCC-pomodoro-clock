c2b9893de67e8258154f114490211bffb48b7bd6
Kaique Borges <rique.kaique@gmail.com>
2022-05-12 00:50:46 -0300

    Create markup and add styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="app-title">25+5 Clock</h1>
+      <div className="clock-options">
+        <div className="clock-option">
+          <h2 id="break-label">Break Length</h2>
+          <div className="number-control">
+            <button id="break-decrement" className="decrease">
+              -
+            </button>
+            <span id="break-length">5</span>
+            <button id="break-increment" className="increse">
+              +
+            </button>
+          </div>
+        </div>
+        <div className="clock-option">
+          <h2 id="session-label">Session Length</h2>
+          <div className="number-control">
+            <button id="session-decrement" className="decrease">
+              -
+            </button>
+            <span id="session-length">25</span>
+            <button id="session-increment" className="increse">
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="clock">
+        <h2 id="timer-label">Session</h2>
+        <p id="time-left">25:00</p> {/* Note the format MM:SS */}
+        <div className="clock-controls">
+          <button id="start_stop" className="clock-controls--start">
+            Start/Stop
+          </button>
+          <button id="reset" className="clock-controls--reset">
+            Reset
+          </button>
+          <audio
+            id="beep"
+            loop
+            src="https://assets.mixkit.co/sfx/download/mixkit-repeating-arcade-beep-1084.wav"
+          ></audio>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
